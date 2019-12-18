7cfee70f6224ac6064a26785122fe7c7b9c54f5e
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-18 12:14:08 -0500

    add state to Login component

--- src/components/registrations/Login.js
+++ src/components/registrations/Login.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 
 class Login extends Component {
 
+    state = {
+        username: '',
+        email: '',
+        password: '',
+        errors: ''
+    }
+
     render() {
         return (
             <div>
                 <h1>Log In</h1>
-               
             </div>
         );
     }
