ca4c297094416ed1912b0b0a7c40bb08d826dbd8
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-18 14:12:07 -0500

    pass delete method to logout click function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 
 const Home = (props) => {
@@ -7,7 +8,18 @@ const Home = (props) => {
     // 1) update the isLoggedIn state in our front - end to false and remove the User data
     // 2) logout the User on the back - end server
     const handleClick = () => {
-        // logout functionality here
+        // to logout the User on the back-end, we make a request to the server's /logout route
+        // triggers the sessions_controller destroy action
+        // also keep the User on the home page by redirecting to the root route
+        // ALWAYS, remember to pass {withCredentials: true}
+        axios.delete('http://localhost:3001/logout', { withCredentials: true })
+            .then(response => {
+
+                props.handleLogout()
+                props.history.push('/')
+
+            })
+            .catch(error => console.log(error))
     }
 
     return (
