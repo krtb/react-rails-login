0e57308c71b06ad0fe37fa32c8bc32dda93408c0
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-18 12:17:01 -0500

    add controlled form to handle user login

--- src/components/registrations/Login.js
+++ src/components/registrations/Login.js
@@ -17,6 +17,36 @@ class Login extends Component {
         return (
             <div>
                 <h1>Log In</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        placeholder="username"
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        placeholder="email"
+                        type="text"
+                        name="email"
+                        value={email}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        placeholder="password"
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={this.handleChange}
+                    />
+                    <button placeholder="submit" type="submit">
+                        Log In
+                    </button>
+                    <div>
+                        or <Link to='/signup'>sign up</Link>
+                    </div>
+
+                </form>
             </div>
         );
     }
