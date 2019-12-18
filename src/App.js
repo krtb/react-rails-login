16526f15942517ee07355ff388760fd461571519
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-18 11:38:56 -0500

    add state, and routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div>
-      React App
-    </div>
-  );
+class App extends Component {
+
+  // use the component’s state to maintain the logged in status of a User
+  // and to store the User data when we request it from the server
+  state = {
+    isLoggedIn: false,
+    user: {}
+  }
+
+  // app won't render itself to DOM
+  // will serve as our router to render all other components
+  // will also manage the application’s state and authentication status
+
+  render() {
+    return (
+      <div>
+
+        <BrowserRouter>
+
+          <Switch>
+
+            <Route exact path='/' component={} />
+            <Route exact path='/login' component={} />
+            <Route exact path='/signup' component={} />
+
+          </Switch>
+
+        </BrowserRouter>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
